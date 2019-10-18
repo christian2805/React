d0a4ee9a0acf1b4446a7c88e053d4a884cbcb999
Chriss <=>
2019-10-18 14:20:47 +0200

    Updates

---
+++ precache-manifest.e8ab508b596da750b2ef08c0f1bbbfcf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5734319984b1bca91e559592be29110",
+    "url": "/React/index.html"
+  },
+  {
+    "revision": "431f5f564dfc39b73173",
+    "url": "/React/static/css/2.dcd591a2.chunk.css"
+  },
+  {
+    "revision": "badf2372c340e20511a5",
+    "url": "/React/static/css/main.8322f3c9.chunk.css"
+  },
+  {
+    "revision": "431f5f564dfc39b73173",
+    "url": "/React/static/js/2.fe8761db.chunk.js"
+  },
+  {
+    "revision": "badf2372c340e20511a5",
+    "url": "/React/static/js/main.ff668bf7.chunk.js"
+  },
+  {
+    "revision": "74ec42058c56fe1b5820",
+    "url": "/React/static/js/runtime-main.ac7a5e1c.js"
+  }
+]);
